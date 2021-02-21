40f54fb86df525c357256b9a732951a78cff9e95
NZB305 <2426866935@qq.com>
2021-02-21 15:42:21 +0800

    学习使用github

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	//char ch = 'A';
+	//printf("%c\n", ch);
+	//return 0;
+	//long num = 100;
+	//printf("%d\n", num);
+	//return 0;
+	float f = 5.0;
+	printf("%f\n", f);
+	return 0;
+}
