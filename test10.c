3fc0379c79c45c4f045c0a53564da8842c4a3af5
NZB305 <2426866935@qq.com>
2021-02-28 08:53:37 +0800

    C

--- test10.c
+++ test10.c
@@ -1,79 +1,172 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<limits.h>
-#include<stdlib.h>//ststem
-#include<string.h>//strcmp
 
+//int main()
+//{
+//	int a = 0x11223344;
+//	//int* pa = &a;
+//	//*pa = 0;
+//	char* pc = &a;
+//	*pc = 0;
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[10]={ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	printf("%d\n", &arr[9] - &arr[0]);//9 指针-指针=中间元素个数
+//	return 0;
+//}
+
+//int my_strlen(char* str)
+//{
+//	char* start = str;
+//	char* end = str;
+//	while (*end != '\0');
+//	{
+//		end++;
+//	}
+//	return end - start;
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int *pa = &a;
+//	int* *ppa = 20;//ppa是二级指针
+//	**ppa = 20;
+//	printf("%d\n", **ppa);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = 30;
+//	int* arr2[3] = { &a, &b, &c };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("%d ", *(arr2[i]));
+//	}
+//	return 0;
+//}
+
+//void Init(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//	}
+//}
+//void Print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//void Reverse(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz-1;
+//	while (left<=right)
+//	{
+//		int tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//	printf("\n");
+//}
+//int main()
+//{
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//Init(arr, sz);
+//	Print(arr, sz);
+//	Reverse(arr, sz);
+//	Print(arr, sz);
+//	return 0;
+//}
+
+//struct结构体关键字	Stu - 结构体标签	strcut Stu - 结构体类型
+// struct Stu
+//{
+//	char name[20];
+//	short age;
+//	char tele[12];
+//	char sex;
+//};
+//int main()
+//{
+//	struct Stu s = { "张三", 20, "152451656", "男" };
+//	return 0;
+//}
+
+//struct S
+//{
+//	int a;
+//	char c;
+//	char arr[20];
+//	double d;
+//};
+//struct T
+//{
+//	char ch[10];
+//	struct S s;
+//	char *pc;
+//};
+//int main()
+//{
+//	char arr[] = "hei\n";
+//	struct T t = { "hehe", { 100, 'w', "hello", 3.14 }, arr };
+//	printf("%s\n", t.ch);//hehe
+//	printf("%s\n", t.s.arr);//hello
+//	printf("%lf\n", t.s.d);//3.140000
+//	printf("%s\n", t.pc);//hei
+//	return 0;
+//}
+
+typedef struct Stu
+{
+	char name[20];
+	short age;
+	char tele[12];
+	char sex[5];
+}Stu;
+void Print1(Stu tmp)
+{
+	printf("name:%s\n", tmp.name);
+	printf("age:%d\n", tmp.age);
+	printf("tele:%s\n", tmp.tele);
+	printf("sex:%s\n", tmp.sex);
+}
+void Print2(Stu* ps)
+{
+	printf("name:%s\n", ps->name);
+	printf("age:%d\n", ps->age);
+	printf("tele:%s\n", ps->tele);
+	printf("sex:%s\n", ps->sex);
+}
 int main()
 {
-	char input[20] = { 0 };//储存数据
-	//关机
-	//system() - 专门用来执行系统命令的
-	system("shutdown -s -t 60");//关机
-
-again:
-	printf("我是猪：");
-	scanf("%s", input);//%s - 字符串
-	if (strcmp(input, "我是猪") == 0)//判断input中放的是不是“我是猪”
-	{
-		system("shutdown -a");
-	}
-	else
-	{
-		goto again;
-	}
+	Stu s = { "李四",40, "1515615", "男" };
+	//打印结构体数据
+	Print1(s);
+	Print2(&s);
 	return 0;
 }
-//int main()
-//{
-//	int arr[] = { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
-//	int i = 0;
-//	int ret = 0;
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	for (i = 0; i < sz; i++)
-//	{
-//		ret = ret^arr[i];
-//	}
-//	printf("单身狗:%d\n", ret);
-//	return 0;
-//}
-//int main()
-//{
-//	int arr[] = { 1, 2, 3, 4, 4, 5, 1, 2, 3 };
-//	//找出单身狗
-//	//暴力求解
-//	int i = 0;
-//	int sz = sizeof(arr) / sizeof(arr[0]);//计算数组的元素个数
-//	for (i = 0; i < sz; i++)
-//	{
-//		//统计arr[i}在arr数组中出现几次
-//		int count = 0;//计数器
-//		int j = 0;
-//		for (j = 0; j < sz; j++)
-//		{
-//			if (arr[i] == arr[j])
-//			{
-//				count++;
-//			}
-//		}
-//
-//		if (count == 1)
-//		{
-//			printf("单身狗:%d\n", arr[i]);
-//			break;
-//		}
-//	}
-//	return 0;
-//}
-//int main()
-//{
-//	int a = 1;
-//	int b = 2;
-//	int c = 0;
-//	printf("a=%d b=%d\n", a, b);
-//	c = a;
-//	a = b;
-//	b = c;
-//	printf("a=%d b=%d\n", a, b);
-//	return 0;
-//}
+
