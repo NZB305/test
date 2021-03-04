b2a05e4cbbf8e70314747ceb02f41fff37277ed9
NZB305 <2426866935@qq.com>
2021-03-04 21:00:00 +0800

    C语言刷题

---
+++ test14.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("Practice makes perfect!");
+//	return 0;
+//}//1
+
+//int main()
+//{
+//	printf("v   v\n v v\n  v");
+//	return 0;
+//}//2
+
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long long));
+//	return 0;
+//}//3
+
+//int main()
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//	return 0;
+//}//6
+
+//int main()
+//{
+//	printf("0%o 0X%X", 1234, 1234);
+//	return 0;
+//}//7
+
+//int main()
+//{
+//	printf("%15d", 0xABCDEF);
+//	return 0;
+//}//8
+
+//int main()
+//{
+//	int a = printf("Hello world!");
+//	printf("\n");
+//	printf("%d", a);
+//	return 0;
+//}//9
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d%d%d", &a, &b, &c);
+//	printf("score1=%d,score2=%d,score3=%d", a, b, c);
+//	return 0;
+//}//10
+
+//17140216;80.845,90.55,100.00
+//int main()
+//{
+//	int a;
+//	float b, c, d;
+//	scanf("%d;%f,%f,%f", &a, &b, &c, &d);
+//	printf("The each subject score of  No. %d is %.2f, %.2f, %.2f.", a, b, c, d);
+//	return 0;
+//}//11
+
+//int main()
+//{
+//	printf("%c%c%c%c%c%c%c%c%c%c%c%c", 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33);
+//	return 0;
+//}//13
+
+//int main()
+//{
+//	char ch='0' ;
+//	while (scanf("%c",&ch)!= EOF)
+//	{
+//		if (ch == '\n')
+//			continue;
+//		else if (('A' <= ch) && (ch <= 'Z'))
+//			printf("%c\n", ch + 32);
+//		else if (('a' <= ch) && (ch <= 'z'))
+//			printf("%c\n", ch - 32);
+//		else
+//			printf("%c\n", ch);
+//	}
+//	return 0;
+//}//48
+
